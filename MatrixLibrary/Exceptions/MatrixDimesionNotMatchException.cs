e79cca62934e2dd07013ce2f75c541a600a15d58
Sachintha Silva <netsachintha@outlook.com>
2022-04-16 04:01:47 +0530

    Code-document modifications and proper naming

---
+++ MatrixLibrary/Exceptions/MatrixDimesionNotMatchException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MatrixLibrary.Exceptions
+{
+    public class MatrixDimesionNotMatchException : Exception
+    {
+        public MatrixDimesionNotMatchException()
+        {
+
+        }
+
+        public MatrixDimesionNotMatchException(string message) : base(message)
+        {
+
+        }
+
+        public MatrixDimesionNotMatchException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
